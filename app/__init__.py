0be36ab39b20b874d8776ee560e4708e921ed098
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-10 17:01:18 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,13 @@ from flask import Flask
 from .extensions import db
 from .routes import main
 from .models import User
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
     #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
